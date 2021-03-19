54dac7c3f118d123c4be71c6378897868d5ff658
unknown <Arunkumar1313>
2021-03-19 19:55:51 +0530

    Hello world

---
+++ 1__Hello world.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//A basic C Program to print Hello World.				
+//Start
+#include<stdio.h>//Including standard input ouput header files from library.
+int main()
+{
+	printf("Hello World");
+	return 0;
+	printf("Hello World");//Here we entered the texted to be printed.
+	return 0;//Returning.
+}
