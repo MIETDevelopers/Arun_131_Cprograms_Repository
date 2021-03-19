c06a49077c3797ee7b66e3af50865f3d7dcb2e0f
unknown <Arunkumar1313>
2021-03-19 19:04:10 +0530

    area perimeter

--- Factorial.c
+++ Factorial.c
@@ -7,14 +7,5 @@ int main() {
 
     // shows error if the user enters a negative integer
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else {
-        for (i = 1; i <= n; ++i) {
-            fact *= i;
-        }
-        printf("Factorial of %d = %llu", n, fact);
-    }
-
-    return 0;
-} 
+        printf("Error! Factorial of a negative number doesn't exist."
  
