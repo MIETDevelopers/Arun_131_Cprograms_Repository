9461def946d0fc6ffd68b2f2b35c5147ded003c2
unknown <Arunkumar1313>
2021-03-20 17:58:48 +0530

    8_Alphabet of vowel or consonant

---
+++ 8_alphabetvowelorconsonant.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    /* Input an alphabet from user */
+    printf("Enter any alphabet: ");/*prinf function calling*/
+    scanf("%c", &ch); /*scanf allows the function to recognise user input as being of an integer data type*/
+
+    /* Switch value of ch */
+    switch(ch)
+    {
+        case 'a': 
+            printf("Vowel");
+            break;
+        case 'e': 
+            printf("Vowel");
+            break;
+        case 'i': 
+            printf("Vowel");
+            break;
+        case 'o': 
+            printf("Vowel");
+            break;
+        case 'u': 
+            printf("Vowel");
+            break;
+        case 'A': 
+            printf("Vowel");
+            break;
+        case 'E': 
+            printf("Vowel");
+            break;
+        case 'I': 
+            printf("Vowel");
+            break;
+        case 'O': 
+            printf("Vowel");
+            break;
+        case 'U': 
+            printf("Vowel");
+            break;
+        default: 
+            printf("Consonant");
+    }
+
+    return 0; /*return statement*/
+}
