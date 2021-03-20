96dc506b4d68dab035d71082153f5e9b245d5880
unknown <Arunkumar1313>
2021-03-20 17:26:28 +0530

    addition file added

---
+++ addition.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int number1, number2, sum;
+	scanf("%d %d", &number1, &number2); //Scanning values given by user.
+	sum=number1+number2; //Processing the sum of two numbers.
+	printf("Sum of 2 numbers= %d", sum); //Printing output.
+	return 0;
+} 
