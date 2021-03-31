3afbf4273d4fa468ceb54a8dfc33c76b5176ea5b
unknown <Arunkumar1313>
2021-03-31 13:58:06 +0530

    Call by value

---
+++ 25_callbyvalue.c
@@ -0,0 +1,14 @@
+Purpose: Program to swap numbers using call by value.
+#include <stdio.h>       //Pre-process directive to include standard input and output functions header file
+int main(){ 
+    int x, y, temp;		    //Variable declarartion of integer data type
+    printf("Enter Value of x "); 		//printf function calling  
+    scanf("%d", &x); 			//scanf function calling
+    printf("\nEnter Value of y ");		//printf function calling  
+    scanf("%d", &y); 			//scanf function calling
+    temp = x; 
+    x = y; 
+    y = temp; 
+    printf("\nAfter Swapping: x = %d, y = %d", x, y);		//printf function calling 
+    return 0; 
+} s
