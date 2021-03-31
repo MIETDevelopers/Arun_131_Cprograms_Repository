52b02c756c9656bdf33cabcd279aeb9ba3c1896b
unknown <Arunkumar1313>
2021-03-31 15:20:51 +0530

    pointer in c

---
+++ pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ #include <stdio.h>    //Header file
+
+int main()
+int main()            //function of the mai body
+{
+    int a;  
+    a = 10;
+    int *p = &a;     // declaring and initializing the pointer
+    int a;           //selecting data type and variable declaration
+    a = 10;          
+    int *p = &a;     //declaring and initializing the pointer
+
+    //prints the value of 'a'
+    printf("%d\n", *p);  
+    printf("%d\n", *&a);  
+    //prints the address of 'a'
+    printf("%u\n", &a);    
+    printf("%u\n", p);     
+    
+    printf("%u\n", &p);    //prints address of 'p'
+    
+    return 0;
+}
